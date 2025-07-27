eda62f60aacd0e50c0f53cf190b3b018e88e1e67
haru <hinagoro5656@gmail.com>
2025-07-27 23:41:38 +0900

    feat: ローカル用にnode環境追加

--- apps/back-azure/src/app.ts
+++ apps/back-azure/src/app.ts
@@ -1,8 +1,17 @@
+import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Azure Functions!"));
-app.get("/abc", (c) => c.json({ message: "Hello from /abc" }));
+app.get("/", (c) => c.text("Hello World!"));
+
+// ローカル開発用のnodeサーバーを起動
+const port = 8000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
 
 export default app;
