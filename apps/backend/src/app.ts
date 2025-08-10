23bafcfb2587ca380ba7e5c8299e89064e8f64cc
haru <hinagoro5656@gmail.com>
2025-08-11 02:13:05 +0900

    refactor: hono-openapi で実装 & Scalarで表示

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,5 +1,7 @@
 import { serve } from "@hono/node-server";
+import { Scalar } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+import { openAPIRouteHandler } from "hono-openapi";
 import ai from "./ai/createObject";
 
 const app = new Hono();
@@ -7,6 +9,41 @@ const app = new Hono();
 app.route("/ai", ai);
 
 app.get("/", (c) => c.text("Hello World!"));
+
+// OpenAPIドキュメントの設定
+app
+  .get(
+    "/openapi.json",
+    openAPIRouteHandler(app, {
+      documentation: {
+        info: {
+          title: "Hono",
+          version: "1.0.0",
+          description: "API for greeting users",
+        },
+      },
+      includeEmptyPaths: false,
+      excludeStaticFile: false,
+      exclude: "",
+      excludeMethods: [],
+      excludeTags: [],
+      defaultOptions: {
+        GET: {},
+        POST: {},
+        PUT: {},
+        DELETE: {},
+        PATCH: {},
+      },
+    }),
+  )
+  .get(
+    "/api/docs",
+    Scalar({
+      url: "/openapi.json",
+      theme: "alternate",
+      pageTitle: "BuildCha API Docs",
+    }),
+  );
 
 // ローカル開発用のnodeサーバーを起動
 const port = 8000;
