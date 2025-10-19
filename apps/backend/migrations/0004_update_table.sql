71bacceb75d6579d791ff927fd74164e1952a7db
たくや <takuyadayo014@gmail.com>
2025-10-19 23:24:05 +0900

    feat: langchain_cacheテーブルの削除とquestsテーブルの再定義

---
+++ apps/backend/migrations/0004_update_table.sql
@@ -0,0 +1,22 @@
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "langchain_cache";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_quests" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "level" INTEGER NOT NULL,
+    "challenge" TEXT,
+    "score" INTEGER NOT NULL,
+    "difficulty" TEXT NOT NULL
+);
+INSERT INTO "new_quests" ("challenge", "createdAt", "difficulty", "id", "level", "name", "score") SELECT "challenge", "createdAt", "difficulty", "id", "level", "name", "score" FROM "quests";
+DROP TABLE "quests";
+ALTER TABLE "new_quests" RENAME TO "quests";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
