f3f4eed9ed809f093707b45e9bdd975d83fe1f98
haru <hinagoro5656@gmail.com>
2025-10-26 16:27:16 +0900

    feat: クエストのサムネイルをr2から取得し、表示

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-68bb760998324b59b97c4622e8ba2d68.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
